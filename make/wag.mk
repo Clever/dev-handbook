ccd318971ba44f3f1d46c29213c7cb3a9519b1c2
Rafael Garcia <rafael.garcia@clever.com>
2016-11-10 01:37:00 +0000

    generate js docs

--- make/wag.mk
+++ make/wag.mk
@@ -1,6 +1,6 @@
 # This is the default Clever Wag Makefile.
 # Please do not alter this file directly.
-WAG_MK_VERSION := 0.2.1
+WAG_MK_VERSION := 0.3.0
 SHELL := /bin/bash
 SYSTEM := $(shell uname -a | cut -d" " -f1 | tr '[:upper:]' '[:lower:]')
 WAG_INSTALLED := $(shell [[ -e "bin/wag" ]] && bin/wag --version)
@@ -21,14 +21,19 @@ bin/wag: ensure-wag-version-set
 	@echo "Using wag version $(WAG_VERSION)"
 	@[[ "$(WAG_VERSION)" != "v$(WAG_INSTALLED)" ]] && echo "Updating wag..." && curl -sL https://github.com/Clever/wag/releases/download/$(WAG_VERSION)/wag-$(WAG_VERSION)-$(SYSTEM)-amd64.tar.gz | tar -xz -C bin || true
 
+jsdoc2md:
+	hash npm 2>/dev/null || (echo "Could not run npm, please install node" && false)
+	hash jsdoc2md 2>/dev/null || npm install -g jsdoc-to-markdown@^2.0.0
+
 # wag-generate-deps installs all dependencies needed for wag generate.
-wag-generate-deps: bin/wag
+wag-generate-deps: bin/wag jsdoc2md
 
 # wag-generate is a target for generating code from a swagger.yml using wag
 # arg1: path to swagger.yml
 # arg2: pkg path
 define wag-generate
 bin/wag -go-package $(2)/gen-go -js-path ./gen-js -file $(1)
+(cd ./gen-js && jsdoc2md index.js types.js > ./README.md)
 go generate $(2)/gen-go/server $(2)/gen-go/client $(2)/gen-go/models
 endef
 
