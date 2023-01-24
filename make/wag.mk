f47c1148cce8d69dbc2744f44048b32e79c03ccf
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2023-01-24 16:37:42 -0500

    version double bump

--- make/wag.mk
+++ make/wag.mk
@@ -1,6 +1,6 @@
 # This is the default Clever Wag Makefile.	
 # Please do not alter this file directly.
-WAG_MK_VERSION := 0.6.1
+WAG_MK_VERSION := 0.6.3
 SHELL := /bin/bash
 SYSTEM := $(shell uname -a | cut -d" " -f1 | tr '[:upper:]' '[:lower:]')
 ifndef CI
