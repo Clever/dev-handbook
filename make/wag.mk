83b07506ff3870953b82cc14d33e3ab64d26ad67
Jake Gutierrez <jake.gutierrez@clever.com>
2026-03-09 15:48:29 -0400

    Update golang.mk to reflect template-wag

--- make/wag.mk
+++ make/wag.mk
@@ -1,16 +1,13 @@
 # This is the default Clever Wag Makefile.
 # Please do not alter this file directly.
-WAG_MK_VERSION := 0.8.0
+WAG_MK_VERSION := 0.8.1
 SHELL := /bin/bash
 SYSTEM := $(shell uname -a | cut -d" " -f1 | tr '[:upper:]' '[:lower:]')
 ifndef CI
 WAG_INSTALLED := $(shell [[ -e "bin/wag" ]] && bin/wag --version)
 WAG_LATEST = $(shell curl --retry 5 -f -s https://api.github.com/repos/Clever/wag/releases/latest | grep tag_name | cut -d\" -f4)
 endif
-.PHONY: wag-update-makefile ensure-wag-version-set wag-generate-deps jsdoc2md
-
-# identify path to jsdoc2md which makes it a file target
-VPATH = node_modules/.bin
+.PHONY: wag-update-makefile ensure-wag-version-set wag-generate-deps
 
 ifndef CI
 ensure-wag-version-set:
@@ -30,29 +27,16 @@ bin/wag: ensure-wag-version-set
 		fi;
 	@if [ -a bin/wag-$(WAG_VERSION)-$(SYSTEM)-amd64.tar.gz ] ; \
 		then \
-			tar xvf bin/wag-$(WAG_VERSION)-$(SYSTEM)-amd64.tar.gz -C bin;\
+			tar xvf bin/wag-$(WAG_VERSION)-$(SYSTEM)-amd64.tar.gz -C bin; \
 			rm bin/wag-$(WAG_VERSION)-$(SYSTEM)-amd64.tar.gz ; \
 		fi;
 	@[[ "$(WAG_VERSION)" != "$(WAG_INSTALLED)" ]] && touch swagger.yml || true
 
-jsdoc2md:
-	hash npm 2>/dev/null || (echo "Could not run npm, please install node" && false)
-	@if [ ! -f ./node_modules/.bin/jsdoc2md ]; then \
-		npm install jsdoc-to-markdown@9.0.0; \
-	elif [ -f ./node_modules/jsdoc-to-markdown/package.json ]; then \
-		INSTALLED_VERSION=$$(node -p "require('./node_modules/jsdoc-to-markdown/package.json').version" 2>/dev/null || echo ""); \
-		if [ "$$INSTALLED_VERSION" != "9.0.0" ]; then \
-			echo "jsdoc-to-markdown version $$INSTALLED_VERSION detected, updating to 9.0.0..."; \
-			npm install jsdoc-to-markdown@9.0.0; \
-		fi; \
-	fi
-
 # wag-generate-deps installs all dependencies needed for wag generate.
-wag-generate-deps: bin/wag jsdoc2md
+wag-generate-deps: bin/wag
 else
 ensure-wag-version-set:
 bin/wag:
-jsdoc2md:
 endif
 
 # wag-yaml-aliases generate code workaround to use YAML aliases in swagger.yml for modules repos
@@ -62,7 +46,6 @@ define wag-yaml-aliases
 @if [ -z "$$CI" ]; then \
 	cat $(1) | python3 -c "import sys, yaml, json; y=yaml.load(sys.stdin.read(), yaml.Loader); print(yaml.dump(y))" > /tmp/swagger.catapult.yml; \
 	bin/wag -output-path ./gen-go -js-path ./gen-js -file /tmp/swagger.catapult.yml; \
-	(cd ./gen-js && ../node_modules/.bin/jsdoc2md index.js types.js > ./README.md); \
 else \
 	echo "skipping wag-yaml-aliases in CI"; \
 fi;
@@ -74,7 +57,6 @@ endef
 define wag-generate
 @if [ -z "$$CI" ]; then \
     bin/wag -go-package $(2)/gen-go -js-path ./gen-js -file $(1); \
-    (cd ./gen-js && ../node_modules/.bin/jsdoc2md index.js types.js > ./README.md); \
 else \
 	echo "skipping wag-generate in CI"; \
 fi;
@@ -85,7 +67,6 @@ endef
 define wag-generate-mod
 @if [ -z "$$CI" ]; then \
     bin/wag -output-path ./gen-go -js-path ./gen-js -file $(1); \
-    (cd ./gen-js && ../node_modules/.bin/jsdoc2md index.js types.js > ./README.md); \
 else \
     echo "skipping wag-generate-mod in CI"; \
 fi;
